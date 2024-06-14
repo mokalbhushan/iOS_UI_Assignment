f7ddff1c3261cbc15f7a5b7afbbf6dff6e5aa252
Bhushan Mokal <bhushanmokal02@gmail.com>
2024-06-15 03:06:37 +0530

    added cell for budget view

--- iOS_UI_Assignment/Controllers/Plan/Cell/BudgetViewCell.swift
+++ iOS_UI_Assignment/Controllers/Plan/Cell/BudgetViewCell.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class BudgetViewCell: UITableViewCell {
 
+    @IBOutlet var budgetTableView: CustomTableView!
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupTableView()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -19,5 +21,27 @@ class BudgetViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+
+    // Setup TableView for Budget Cell
+    func setupTableView() {
+        budgetTableView.delegate = self
+        budgetTableView.dataSource = self
+        self.budgetTableView.register(UINib(nibName: "BudgetItemCell", bundle: nil), forCellReuseIdentifier: "BudgetItemCell")
+        self.budgetTableView.reloadData()
+    }
+
+}
+
+
+extension BudgetViewCell: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BudgetItemCell", for: indexPath) as? BudgetItemCell else { return UITableViewCell()}
+        return cell
+    }
+    
+
 }
---
+++ iOS_UI_Assignment/Utilities/UITableView.swift
@@ -0,0 +1,34 @@
+//
+//  UITableView.swift
+//  iOS_UI_Assignment
+//
+//  Created by Bhushan Mokal on 15/06/24.
+//
+
+import Foundation
+import UIKit
+
+class CustomTableView: UITableView {
+
+    private let maxHeight = CGFloat(2000)
+    private let minHeight = CGFloat(65)
+
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        if bounds.size != intrinsicContentSize {
+            invalidateIntrinsicContentSize()
+        }
+    }
+
+    override public var intrinsicContentSize: CGSize {
+        layoutIfNeeded()
+        if contentSize.height > maxHeight {
+            return CGSize(width: contentSize.width, height: maxHeight)
+        } else if contentSize.height < minHeight {
+            return CGSize(width: contentSize.width, height: minHeight)
+        } else {
+            return CGSize(width: contentSize.width, height: contentSize.height)
+        }
+    }
+
+}
