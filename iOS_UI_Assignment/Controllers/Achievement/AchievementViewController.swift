38b50fb10d2e938ba86f84b6b45fb87efa1d04c5
Bhushan Mokal <bhushanmokal02@gmail.com>
2024-06-14 22:40:26 +0530

    done with achievement screen

--- iOS_UI_Assignment/Controllers/Achievement/AchievementViewController.swift
+++ iOS_UI_Assignment/Controllers/Achievement/AchievementViewController.swift
@@ -13,13 +13,28 @@ class AchievementViewController: UIViewController {
         return AchievementViewController(nibName: "AchievementViewController", bundle: nil)
     }
 
+// MARK: UI Outlets
+
+    @IBOutlet var vRImageView: UIImageView!
+    @IBOutlet var tickMarkImageView: UIImageView!
+    @IBOutlet var faceBookButton: UIButton!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.navigationBar.isHidden = true
+    }
+
+    // MARK: UI setup
+    func setupUI() {
+        tickMarkImageView.layer.cornerRadius = self.tickMarkImageView.bounds.width / 2
+        vRImageView.layer.cornerRadius = 10
+        faceBookButton.layer.cornerRadius = 10
     }
 
     @IBAction func closeButtonTapped(_ sender: Any) {
