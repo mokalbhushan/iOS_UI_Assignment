6ebfbd6865d195e80b2744071d1897d59eb5556a
Bhushan Mokal <bhushanmokal02@gmail.com>
2024-06-15 11:51:29 +0530

    added collectionCell for workout

---
+++ iOS_UI_Assignment/Controllers/Workout/Cell/WorkoutCell.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutCell.swift
+//  iOS_UI_Assignment
+//
+//  Created by Bhushan Mokal on 15/06/24.
+//
+
+import UIKit
+
+class WorkoutCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- iOS_UI_Assignment/Controllers/Workout/WorkoutViewController.swift
+++ iOS_UI_Assignment/Controllers/Workout/WorkoutViewController.swift
@@ -9,23 +9,49 @@ import UIKit
 
 class WorkoutViewController: UIViewController {
 
+    @IBOutlet var workOutCollectionView: UICollectionView!
+
     static func loadFromNib() -> WorkoutViewController {
         return WorkoutViewController(nibName: "WorkoutViewController", bundle: nil)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.navigationController?.navigationBar.isHidden = true
-        // Do any additional setup after loading the view.
+        setupUI()
     }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        self.navigationController?.navigationBar.isHidden = true
         navigationItem.hidesBackButton = true
 
+    }
+
+    // Setup UI
+    func setupUI() {
+        self.workOutCollectionView.dataSource = self
+        self.workOutCollectionView.delegate = self
+        self.workOutCollectionView.register(UINib(nibName: "WorkoutCell", bundle: nil), forCellWithReuseIdentifier: "WorkoutCell")
+        self.workOutCollectionView.reloadData()
     }
 
     @IBAction func endWorkoutButtonTapped(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
+}
+
+extension WorkoutViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WorkoutCell", for: indexPath) as? WorkoutCell else {return UICollectionViewCell()}
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height)
+    }
+
 }
