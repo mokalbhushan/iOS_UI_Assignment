f6abf99a706dd25607befe399c03d4d0ed6f39d3
Bhushan Mokal <bhushanmokal02@gmail.com>
2024-06-15 12:28:53 +0530

    added pageControl and Fixed UI

--- iOS_UI_Assignment/Controllers/Workout/WorkoutViewController.swift
+++ iOS_UI_Assignment/Controllers/Workout/WorkoutViewController.swift
@@ -11,13 +11,23 @@ class WorkoutViewController: UIViewController {
 
     @IBOutlet var workOutCollectionView: UICollectionView!
 
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var workoutConuntLabel: UILabel!
+    @IBOutlet var pageControl: UIPageControl!
+
+    @IBOutlet var endWorkoutButton: UIButton!
+    @IBOutlet var nextButton: UIButton!
+
     static func loadFromNib() -> WorkoutViewController {
         return WorkoutViewController(nibName: "WorkoutViewController", bundle: nil)
     }
 
+    private var numberOfWorkoutScreen: Int = 4
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        pageControl.numberOfPages = numberOfWorkoutScreen
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -29,10 +39,28 @@ class WorkoutViewController: UIViewController {
 
     // Setup UI
     func setupUI() {
+        setupWorkoutCountLabel()
+        self.playButton.layer.cornerRadius = playButton.bounds.width / 2
+        self.endWorkoutButton.layer.cornerRadius = 10
+        self.nextButton.layer.cornerRadius = 10
         self.workOutCollectionView.dataSource = self
         self.workOutCollectionView.delegate = self
         self.workOutCollectionView.register(UINib(nibName: "WorkoutCell", bundle: nil), forCellWithReuseIdentifier: "WorkoutCell")
         self.workOutCollectionView.reloadData()
+    }
+
+    // Set the attributed string to the label
+    func setupWorkoutCountLabel() {
+        let text = "15/32"
+        let attributedString = NSMutableAttributedString(string: text)
+        if let slashRange = text.range(of: "/") {
+            let nsRange = NSRange(slashRange, in: text)
+            let start = nsRange.location
+            let length = text.count - start
+            let range = NSRange(location: start, length: length)
+            attributedString.addAttribute(.foregroundColor, value: UIColor.lightGray, range: range)
+        }
+        workoutConuntLabel.attributedText = attributedString
     }
 
     @IBAction func endWorkoutButtonTapped(_ sender: Any) {
@@ -48,12 +76,16 @@ class WorkoutViewController: UIViewController {
 
 extension WorkoutViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return numberOfWorkoutScreen
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WorkoutCell", for: indexPath) as? WorkoutCell else {return UICollectionViewCell()}
         return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        self.pageControl.currentPage = indexPath.row
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
